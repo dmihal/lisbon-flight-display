ade6c93c67931680df846d651b2274b046727bb9
David Mihal <david@dmihal.com>
2024-07-31 19:08:41 +0100

    Increase font sizes

--- app/components/Flight.tsx
+++ app/components/Flight.tsx
@@ -13,18 +13,18 @@ interface FlightProps {
 export default function Flight({ airport, inbound, number, plane, airline, distance, speed }: FlightProps) {
   return (
     <div className="flex items-center justify-between bg-gray-800 text-white p-4 rounded-lg shadow-lg">
-      <div className="text-4xl w-16">
+      <div className="text-5xl w-16">
         {inbound ? '🛬' : '🛫'}
       </div>
-      <div className="text-xl w-24">{number}</div>
-      <div className="flex-grow text-left text-3xl font-bold">
+      <div className="text-2xl font-medium w-36 text-center">{number}</div>
+      <div className="flex-grow text-left text-4xl font-bold">
         {airport}
       </div>
-      <div className="flex flex-col text-center w-64">
-        <div className="text-lg">{airline}</div>
-        <div className="text-lg">{plane}</div>
+      <div className="flex flex-col text-center w-72">
+        <div className="text-xl">{airline}</div>
+        <div className="text-xl">{plane}</div>
       </div>
-      <div className="text-2xl font-medium w-24 text-right">
+      <div className="text-3xl font-medium w-36 text-right">
         <Counter value={distance} speed={speed} />km
       </div>
     </div>
