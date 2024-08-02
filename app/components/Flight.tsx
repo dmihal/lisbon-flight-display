bdde9461d82235f275a62d75305e9220d8a3c100
David Mihal <david@dmihal.com>
2024-08-02 18:26:14 +0100

    Emojis next to cities

--- app/components/Flight.tsx
+++ app/components/Flight.tsx
@@ -1,7 +1,8 @@
+import { Airport } from "../utils/flights";
 import { Counter } from "./Counter";
 
 interface FlightProps {
-  airport: string;
+  airport: Airport;
   inbound: boolean;
   number: string;
   plane: string;
@@ -18,7 +19,7 @@ export default function Flight({ airport, inbound, number, plane, airline, dista
       </div>
       <div className="text-2xl font-medium text-center md:row-span-2 flex items-center">{number}</div>
       <div className="text-3xl col-span-2 md:col-span-1 md:text-5xl font-bold md:row-span-2 flex items-center">
-        {airport}
+        {airport.flag} {airport.name}
       </div>
       <div className="md:text-3xl text-center text-xl">{airline}</div>
       <div className="md:col-start-4 md:row-start-2 md:text-2xl text-center text-xl">{plane}</div>
