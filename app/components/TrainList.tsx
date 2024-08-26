ce095521ae77b633a0ebef29649417f38c40e742
David Mihal <david@dmihal.com>
2024-08-26 20:07:42 +0100

    Show trains based on IP data

--- app/components/TrainList.tsx
+++ app/components/TrainList.tsx
@@ -1,15 +1,62 @@
 "use client"
 import { useEffect, useState } from "react";
 import { isPointInQuadrilateral, Point } from "../utils/geo";
+import { Counter } from "./Counter";
 
 const stations = [
-  // "pragal",
-  // "campolide",
+  "pragal",
+  "campolide",
   "alcantara_mar",
   "belem",
 ];
 
 const MINUTE = 60 * 1000;
+
+function formatTime(date: Date): string {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+  return `${formattedHours}:${formattedMinutes}`;
+}
+
+function isVisible(train: any): boolean {
+  const percentage = (train.arrivalTime.getTime() - Date.now()) / (train.arrivalTime.getTime() - train.departureTime.getTime());
+
+  switch (train.destination) {
+    case 'CASCAIS':
+      return percentage > 0.5 && percentage < 0.7;
+    case 'CAIS DO SODRÉ':
+      return percentage > 0 && percentage < 0.8;
+    case 'COINA':
+    case 'SETÚBAL':
+      return percentage > 0.1 && percentage < 0.6;
+    case 'ROMA-AREEIRO':
+      return percentage > 0.2 && percentage < 0.6;
+    default:
+      return false;
+  }
+}
+
+
+function getDirection(train: any): string {
+  switch (train.destination) {
+    case 'CASCAIS':
+      return 'west';
+    case 'CAIS DO SODRÉ':
+      return 'east';
+    case 'COINA':
+    case 'SETÚBAL':
+      return 'south';
+    case 'ROMA-AREEIRO':
+      return 'north';
+    default:
+      return '';
+  }
+}
+
+const percentage = (train: any) => (train.arrivalTime.getTime() - Date.now()) / (train.arrivalTime.getTime() - train.departureTime.getTime());
 
 export default function TrainList() {
   const [trains, setTrains] = useState<any[]>([]);
@@ -31,14 +78,49 @@ export default function TrainList() {
           }
         }
 
+        const line = station == 'alcantara_mar' || station == 'belem' ? 'cascais' : 'fertagus';
+
         console.log(train, departureTime);
-        return { ...train, nextStop: station, departureTime };
+        return { ...train, nextStop: station, departureTime, line };
       });
     })
     .flat()
-    .filter((train: any) => train.departureTime > new Date() && train.departureTime < new Date(Date.now() + 30 * MINUTE));
-    
-    setTrains(nextTrains);
+    .filter((train: any) => {
+      const tenMinAgo = new Date(Date.now() - 10 * MINUTE);
+      const twentyMinAhead = new Date(Date.now() + 20 * MINUTE);
+      return train.departureTime > tenMinAgo && train.departureTime < twentyMinAhead;
+    });
+
+    const trainMap: { [key: number]: any } = {};
+    nextTrains.forEach((train: any) => {
+      if (!trainMap[train.NComboio1]) {
+        trainMap[train.NComboio1] = {
+          number: train.NComboio1,
+          line: train.line,
+          destination: train.NomeEstacaoDestino,
+          nextStop: train.nextStop,
+          tmpTime: train.departureTime,
+        };
+      } else {
+        const { tmpTime, ...prevTrain } = trainMap[train.NComboio1];
+        const [departureTime, arrivalTime] = tmpTime < train.departureTime
+          ? [tmpTime, train.departureTime]
+          : [train.departureTime, tmpTime];
+        
+        
+        trainMap[train.NComboio1] = {
+          ...prevTrain,
+          departureTime,
+          arrivalTime,
+        };
+      }
+    });
+
+    const sortedTrains = Object.values(trainMap)
+      .filter((train: any) => !train.tmpTime)
+      .sort((a: any, b: any) => a.departureTime - b.departureTime);
+
+    setTrains(sortedTrains);
   };
 
   useEffect(() => {
@@ -53,7 +135,13 @@ export default function TrainList() {
     <div>
       Trains
       <ul>
-        {trains.map(train => <li>🚂<pre>{train.NComboio1} - {train.NomeEstacaoDestino} - {train.departureTime.toString()}</pre></li>)}
+        {trains.map(train => (
+          <li>
+            <div>🚂{train.number}: {getDirection(train)} - {train.line} - {train.destination}: {isVisible(train) && 'Visible'}</div>
+            <pre>{formatTime(train.departureTime)} - {formatTime(train.arrivalTime)} ({percentage(train)})</pre>
+            <Counter value={(train.arrivalTime.getTime() - Date.now()) / 60 / 1000} speed={-1/60} />
+          </li>
+        ))}
       </ul>
     </div>
   )
