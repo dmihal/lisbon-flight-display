04d301c17ffaf627111733a62d9fb9b241eb6cf8
David Mihal <david@dmihal.com>
2024-08-29 13:33:09 +0100

    Show train box

--- app/components/TrainList.tsx
+++ app/components/TrainList.tsx
@@ -2,6 +2,9 @@
 import { useEffect, useState } from "react";
 import { isPointInQuadrilateral, Point } from "../utils/geo";
 import { Counter } from "./Counter";
+import Train from "./Train";
+import { getDirection, isVisible, percentage } from "../utils/trains";
+import { formatTime } from "../utils/time";
 
 const stations = [
   "pragal",
@@ -12,54 +15,9 @@ const stations = [
 
 const MINUTE = 60 * 1000;
 
-function formatTime(date: Date): string {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-
-  return `${formattedHours}:${formattedMinutes}`;
-}
-
-function isVisible(train: any): boolean {
-  const percentage = (train.arrivalTime.getTime() - Date.now()) / (train.arrivalTime.getTime() - train.departureTime.getTime());
-
-  switch (train.destination) {
-    case 'CASCAIS':
-      return percentage > 0.5 && percentage < 0.7;
-    case 'CAIS DO SODRÉ':
-      return percentage > 0 && percentage < 0.8;
-    case 'COINA':
-    case 'SETÚBAL':
-      return percentage > 0.1 && percentage < 0.6;
-    case 'ROMA-AREEIRO':
-      return percentage > 0.2 && percentage < 0.6;
-    default:
-      return false;
-  }
-}
-
-
-function getDirection(train: any): string {
-  switch (train.destination) {
-    case 'CASCAIS':
-      return 'west';
-    case 'CAIS DO SODRÉ':
-      return 'east';
-    case 'COINA':
-    case 'SETÚBAL':
-      return 'south';
-    case 'ROMA-AREEIRO':
-      return 'north';
-    default:
-      return '';
-  }
-}
-
-const percentage = (train: any) => (train.arrivalTime.getTime() - Date.now()) / (train.arrivalTime.getTime() - train.departureTime.getTime());
-
 export default function TrainList() {
   const [trains, setTrains] = useState<any[]>([]);
+  const [showData, setShowData] = useState(false);
 
   const refreshTrains = async () => {
     const response = await fetch("/api/trains");
@@ -134,15 +92,28 @@ export default function TrainList() {
   return (
     <div>
       Trains
-      <ul>
-        {trains.map(train => (
-          <li key={train.number}>
-            <div>🚂{train.number}: {getDirection(train)} - {train.line} - {train.destination}: {isVisible(train) && 'Visible'}</div>
-            <pre>{formatTime(train.departureTime)} - {formatTime(train.arrivalTime)} ({percentage(train)})</pre>
-            <Counter value={(train.arrivalTime.getTime() - Date.now()) / 60 / 1000} speed={-1/60} />
-          </li>
+      <div>
+        {trains.filter(train => isVisible(train) && train.line == 'fertagus').map(train => (
+          <Train key={train.number} train={train} />
         ))}
-      </ul>
+      </div>
+
+      <label>
+        <input type="checkbox" checked={showData} onChange={() => setShowData(!showData)} />
+        Show data
+      </label>
+
+      {showData && (
+        <ul>
+          {trains.map(train => (
+            <li key={train.number}>
+              <div>🚂{train.number}: {getDirection(train)} - {train.line} - {train.destination}: {isVisible(train) && 'Visible'}</div>
+              <pre>{formatTime(train.departureTime)} - {formatTime(train.arrivalTime)} ({percentage(train)})</pre>
+              <Counter value={(train.arrivalTime.getTime() - Date.now()) / 60 / 1000} speed={-1/60} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
